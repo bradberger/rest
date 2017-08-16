760e6f8a3d21ed0d5d421e6fe016273d21366e31
Brad Berger <brad@bradb.net>
2017-08-16 14:10:11 +0200

    Allows byte slices to be passed to JSON
    Byte slices passed to JSON() now skip encoding

--- writers.go
+++ writers.go
@@ -64,10 +64,13 @@ func JSON(ctx context.Context, code int, data interface{}) error {
 	w := ResponseWriter(ctx)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
-	if err := json.NewEncoder(w).Encode(data); err != nil {
+	switch data.(type) {
+	case []byte:
+		_, err := w.Write(data.([]byte))
 		return err
+	default:
+		return json.NewEncoder(w).Encode(data)
 	}
-	return nil
 }
 
 // XML writes the XML encoded data to the HTTP connection
